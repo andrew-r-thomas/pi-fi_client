932738d43276dcefb543c59eb0f119c63c586a8d
Andrew Thomas <andrew@andrewrthomas.com>
2025-02-16 20:57:06 -0800

    getting somewhere

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,93 @@
 <script lang="ts">
+  /*
+
+    TODO: experiment with doing things vanilla js style and moving as much as
+    possible to the rust side of things
+
+  */
+
   import { open } from "@tauri-apps/plugin-dialog";
   import { upload } from "@tauri-apps/plugin-upload";
+  import { readDir, stat } from "@tauri-apps/plugin-fs";
+  import { extname, join } from "@tauri-apps/api/path";
+  import { fetch } from "@tauri-apps/plugin-http";
 
-  const addTracks = async () => {
+  import { Plus, LoaderCircle } from "lucide-svelte";
+
+  let uploading = $state(false);
+  let uploadProgress = $state(0);
+  let uploadTotal = $state(0);
+
+  type Album = {
+    id: number;
+    title: string;
+  };
+
+  let waiting = $state(false);
+  let album = $state<Album | null>(null);
+
+  const addAlbum = async () => {
     const selected = await open({
       multiple: false,
       directory: true,
     });
     if (!selected) return;
 
-    /*
+    const dir = await readDir(selected);
 
-    NOTE:
-    - ok so just for you to pick up where you left off, right now we want to focus
-      on full library ingest, as we want our metadata system to be good enough
-      that most users will do a big ingest up front, and then add albums
-      individually as they buy
-    - we want to nail down how much to do on the client vs the server
+    let tracks = [];
+    for (const entry of dir) {
+      const path = await join(selected, entry.name);
+      const ext = await extname(path);
+      if (ext != "flac") continue;
 
-     */
-
-    let proms = [];
-    let headers = new Map<string, string>();
-    headers.set("Content-Type", "audio/flac");
-    for (let path of selected) {
-      let p = upload(
-        "http://localhost:8080/upload",
-        path,
-        ({ progress, total }) => {
-          console.log(`uploaded ${progress}/${total} of ${path}`);
-        },
-        headers,
-      );
-
-      proms.push(p);
+      const md = await stat(path);
+      uploadTotal += md.size;
+      tracks.push(path);
     }
 
-    let resps = await Promise.all(proms);
-    console.log(`resps: ${resps}`);
+    waiting = true;
+    const albumId = await upload(
+      "http://localhost:8080/upload-track",
+      tracks[0],
+    );
+    console.log(`album id: ${albumId}`);
+    const albumResp = await fetch(`http://localhost:8080/album?id=${albumId}`);
+    album = await albumResp.json();
+    waiting = false;
+
+    uploading = true;
+    await new Promise((r) => setTimeout(r, 1000));
+    uploading = false;
+
+    uploadTotal = 0;
+    uploadProgress = 0;
   };
 </script>
 
 <main class="bg-slate-900 h-screen w-screen text-white flex flex-col p-4">
-  <h1>pi-fi</h1>
-  <button onclick={addTracks} class="border-red-600 border-2 h-fit w-fit">
-    ingest
+  <h1 class="text-4xl font-bold">Library</h1>
+  <button
+    onclick={addAlbum}
+    disabled={waiting}
+    class="border-indigo-700 border-2 h-fit w-fit flex flex-row space-x-2 px-4 py-2 rounded-md"
+  >
+    {#if waiting}
+      <LoaderCircle class="animate-spin" />
+    {:else}
+      <Plus />
+    {/if}
+    <span>add album</span>
   </button>
+  {#if album !== null}
+    <div class="flex flex-col">
+      <p>{album.title}</p>
+      {#if uploading}
+        <p class="flex flex-col">
+          <span class="font-bold">progress</span>
+          <span>{(uploadProgress / uploadTotal) * 100}%</span>
+        </p>
+      {/if}
+    </div>
+  {/if}
 </main>
---
+++ src/routes/album-view/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  const { id }: { id: number } = $props();
+</script>
+
+<main>
+  <h1>{id}</h1>
+</main>
